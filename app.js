98333d326df3f26322de43b674a68fc229e8b50e
Kevin Chu <kevinxchu@gmail.com>
2018-01-16 17:37:00 +0900

    add post detail and css

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-var urlencdedParser = bodyParser.urlencoded({ extended: false });
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 var data = require('./models/post');
+
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.get('/', function(req, res){
@@ -13,6 +15,25 @@ app.get('/', function(req, res){
 
 app.get('/posts', function(req, res){
     res.render('posts', {posts: data});
+    console.log(req.path);
+});
+
+app.get('/posts/:post_id', function(req, res){
+    res.render('post_detail', {post: data[req.params.post_id]});
+    console.log(req.params.post_id);
+    console.log('hello');
+});
+
+    
+
+app.get('/post_new', function(req, res){
+    res.render('post_new');
+});
+
+app.post('/post_new', urlencodedParser, function(req, res){
+    data.push(req.body);
+    res.render('posts', {posts: data});
+    res.json(data);
 });
 
 
