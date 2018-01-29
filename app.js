d61852616ab9e1bdc62c4b307ffc1c8573865de7
Kevin Chu <kevinxchu@gmail.com>
2018-01-29 16:59:15 +0900

    fix error handling and adhere to style guide

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express       = require('express');
-const router    = require('./controllers/controller.js');
+const postController    = require('./controllers/postController.js');
 
 const app = express();
 
-app.use('', router);
+app.use('', postController);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
--- controllers/postController.js
+++ controllers/postController.js
@@ -1,7 +1,7 @@
 const bodyParser    = require('body-parser');
 const mongoose      = require('mongoose');
 const Post          = require('../models/post');
-const service       = require('../services/service');
+const PostService   = require('../services/PostService');
 const express       = require('express');
 
 
@@ -13,19 +13,23 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://test:test@ds257627.mlab.com:57627/first-blog-mern', { useMongoClient: true });
 
-const dataService = new service();
+const postService = new PostService();
 
 
 router.get(['/', '/posts'], (req, res) => {
-    dataService.getAllPosts().then(posts => {
+    postService.getAllPosts().then(posts => {
         res.render('posts', {posts: posts});
-    })/*.catch(res.status(500).send('Service Unavailable'));*/
+    }).catch(() => {
+        res.status(500).send('Service Unavailable')
+    });
 });
 
 router.get('/posts/:post_id', (req, res) => {
-    dataService.getOnePost(req.params.post_id).then((post) => {
+    postService.getOnePost(req.params.post_id).then((post) => {
         res.render('post_detail', {post: post});
-    }).catch(res.status(500).send('Service Unavailable'));
+    }).catch(() => {
+        res.status(500).send('Service Unavailable')
+    });
 });
 
 router.get('/post_new', (req, res) => {
@@ -34,9 +38,11 @@ router.get('/post_new', (req, res) => {
 
 router.post('/post_new', urlencodedParser, (req, res) => {
     // get data from view and add to mongodb
-    dataService.postNewPost(req.body).then((post) => {
+    postService.postNewPost(req.body).then((post) => {
         res.redirect('posts');
-    }).catch(res.status(500).send('Service Unavailable'));
+    }).catch(() => {
+        res.status(500).send('Service Unavailable')
+    });
 });
 
 
