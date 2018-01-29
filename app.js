3a04b80e2688c84ed3f9ccdd10ece41939845fe7
Kevin Chu <kevinxchu@gmail.com>
2018-01-29 16:40:13 +0900

    change to express router setup, but has error

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express       = require('express');
-const controller    = require('./controllers/controller.js');
+const router    = require('./controllers/controller.js');
 
 const app = express();
+
+app.use('', router);
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
-controller(app);
 
 app.listen(3000);
--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,10 @@ const bodyParser    = require('body-parser');
 const mongoose      = require('mongoose');
 const Post          = require('../models/post');
 const service       = require('../services/service');
+const express       = require('express');
+
+
+const router        = express.Router();
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -10,33 +14,30 @@ mongoose.Promise = require('bluebird');
 mongoose.connect('mongodb://test:test@ds257627.mlab.com:57627/first-blog-mern', { useMongoClient: true });
 
 const dataService = new service();
-module.exports = (app) => {
-    
 
 
-    app.get(['/', '/posts'], (req, res) => {
-        dataService.getAllPosts().then(posts => {
-            res.render('posts', {posts: posts});
-        }).catch(
-            res.status(500);
-        );
-    });
-    
-    app.get('/posts/:post_id', (req, res) => {
-        dataService.getOnePost(req.params.post_id).then((post) => {
-            res.render('post_detail', {post: post});
-        });
-    });
+router.get(['/', '/posts'], (req, res) => {
+    dataService.getAllPosts().then(posts => {
+        res.render('posts', {posts: posts});
+    })/*.catch(res.status(500).send('Service Unavailable'));*/
+});
 
-    app.get('/post_new', (req, res) => {
-        res.render('post_new');
-    });
+router.get('/posts/:post_id', (req, res) => {
+    dataService.getOnePost(req.params.post_id).then((post) => {
+        res.render('post_detail', {post: post});
+    }).catch(res.status(500).send('Service Unavailable'));
+});
 
-    app.post('/post_new', urlencodedParser, (req, res) => {
-        // get data from view and add to mongodb
-        dataService.postNewPost(req.body).then((post) => {
-            res.redirect('posts');
-        });
-    });
+router.get('/post_new', (req, res) => {
+    res.render('post_new');
+});
 
-};
+router.post('/post_new', urlencodedParser, (req, res) => {
+    // get data from view and add to mongodb
+    dataService.postNewPost(req.body).then((post) => {
+        res.redirect('posts');
+    }).catch(res.status(500).send('Service Unavailable'));
+});
+
+
+module.exports = router;
