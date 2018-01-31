d9e00eb1de404859836a617a79a43758e2ae5a82
Kevin Chu <kevinxchu@gmail.com>
2018-02-01 02:34:04 +0900

    lint postServiceTest

--- test/postServiceTest.js
+++ test/postServiceTest.js
@@ -1,19 +1,18 @@
-const mocha = require('mocha');
+/* global it, describe */
 const sinon = require('sinon');
 const assert = require('assert');
 const Post = require('../models/Post');
-const mongoose = require('../mongoose');
 
 const PostService = require('../services/PostService');
 
 describe('PostService', () => {
-    describe('getOnePost(post_id)', () => {
-        it('should return correct post', () => {
-            const stub = sinon.stub(Post, 'find').returns(5);
-            const sut = new PostService();
-            post_id = sut.getOnePost(5);
-            assert.equal(post_id, 5);
-            stub.restore();
-        });
+  describe('getOnePost(post_id)', () => {
+    it('should return correct post', () => {
+      const stub = sinon.stub(Post, 'find').returns(5);
+      const sut = new PostService();
+      const postId = sut.getOnePost(5);
+      assert.equal(postId, 5);
+      stub.restore();
     });
+  });
 });
