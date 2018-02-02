29fe47c94940cc8606e30b5b22fa2cb2b0a1d7bd
Kevin Chu <kevinxchu@gmail.com>
2018-02-03 01:39:30 +0900

    add unit tests for all service methods

--- test/postServiceTest.js
+++ test/postServiceTest.js
@@ -2,16 +2,36 @@
 const sinon = require('sinon');
 const assert = require('assert');
 const Post = require('../models/Post');
-
 const PostService = require('../services/PostService');
 
 describe('PostService', () => {
   describe('getOnePost(post_id)', () => {
     it('should return correct post', () => {
-      const stub = sinon.stub(Post, 'find').returns(5);
-      const sut = new PostService();
-      const postId = sut.getOnePost(5);
-      assert.equal(postId, 5);
+      const stub = sinon.stub(Post, 'findById').returns(5);
+      const post = PostService.getOnePost(5);
+      assert.equal(post, 5);
+      stub.restore();
+    });
+    it('should be called with correct parameters', () => {
+      const stub = sinon.stub(Post, 'findById');
+      PostService.getOnePost(5);
+      sinon.assert.calledWith(stub, 5);
+      stub.restore();
+    });
+  });
+  describe('getAllPosts()', () => {
+    it('should be called with no parameters', () => {
+      const stub = sinon.stub(Post, 'find');
+      PostService.getAllPosts();
+      sinon.assert.calledWith(stub);
+      stub.restore();
+    });
+  });
+  describe('createNewPost(title, author, body)', () => {
+    it('should be called with correct parameters', () => {
+      const stub = sinon.stub(Post, 'create');
+      PostService.createNewPost('intro', 'kevin', 'hello');
+      sinon.assert.calledWith(stub, { title: 'intro', author: 'kevin', body: 'hello' });
       stub.restore();
     });
   });
