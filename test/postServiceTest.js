287d31b43d8ab739b81d3d70eeb8d1c089652a5c
Kevin Chu <kevinxchu@gmail.com>
2018-02-03 12:12:23 +0900

    remove out of scope tests and refine getOnePost test

--- test/postServiceTest.js
+++ test/postServiceTest.js
@@ -7,31 +7,15 @@ const PostService = require('../services/PostService');
 describe('PostService', () => {
   describe('getOnePost(post_id)', () => {
     it('should return correct post', () => {
-      const stub = sinon.stub(Post, 'findById').returns(5);
-      const post = PostService.getOnePost(5);
-      assert.equal(post, 5);
+      const stub = sinon.stub(Post, 'findById').returns('post');
+      const post = PostService.getOnePost('postId');
+      assert.equal(post, 'post');
       stub.restore();
     });
-    it('should be called with correct parameters', () => {
+    it('should call function findById with correct parameters', () => {
       const stub = sinon.stub(Post, 'findById');
       PostService.getOnePost(5);
       sinon.assert.calledWith(stub, 5);
-      stub.restore();
-    });
-  });
-  describe('getAllPosts()', () => {
-    it('should be called with no parameters', () => {
-      const stub = sinon.stub(Post, 'find');
-      PostService.getAllPosts();
-      sinon.assert.calledWith(stub);
-      stub.restore();
-    });
-  });
-  describe('createNewPost(title, author, body)', () => {
-    it('should be called with correct parameters', () => {
-      const stub = sinon.stub(Post, 'create');
-      PostService.createNewPost('intro', 'kevin', 'hello');
-      sinon.assert.calledWith(stub, { title: 'intro', author: 'kevin', body: 'hello' });
       stub.restore();
     });
   });
