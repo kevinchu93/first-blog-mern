cd47ec41b667d90d661f12f8e43a5112d4858384
Kevin Chu <kevinxchu@gmail.com>
2018-02-04 14:54:16 +0900

    initial commit for postController tests

---
+++ test/postControllerTest.js
@@ -0,0 +1,7 @@
+const mocha = require('mocha');
+const sinon = require('sinon');
+const assert = require('assert');
+
+//describe('postController', () => {
+//  describe('router.get', () => {
+//    it('should 
