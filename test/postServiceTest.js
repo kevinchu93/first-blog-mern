c51ad8deda2b8d403d545044be5f27ca645b1dec
Kevin Chu <kevinxchu@gmail.com>
2018-02-07 14:16:49 +0900

    change class to function in postService, and modify affected files

--- test/postServiceTest.js
+++ test/postServiceTest.js
@@ -2,20 +2,20 @@
 const sinon = require('sinon');
 const assert = require('assert');
 const Post = require('../models/Post');
-const PostService = require('../services/PostService');
+const postService = require('../services/postService');
 
-describe('PostService', () => {
+describe('postService', () => {
   describe('getOnePost(post_id)', () => {
     it('should call function "findById" with correct parameters', () => {
       const stub = sinon.stub(Post, 'findById');
-      PostService.getOnePost(5);
+      postService.getOnePost(5);
       sinon.assert.calledWith(stub, 5);
       stub.restore();
     });
     it('should return correct post', () => {
       const expected = sinon.mock();
       const stub = sinon.stub(Post, 'findById').returns(expected);
-      const post = PostService.getOnePost(5);
+      const post = postService.getOnePost(5);
       assert.equal(post, expected);
       stub.restore();
     });
@@ -23,14 +23,14 @@ describe('PostService', () => {
   describe('getAllPosts()', () => {
     it('should call function "find" with no parameters', () => {
       const stub = sinon.stub(Post, 'find');
-      PostService.getAllPosts();
+      postService.getAllPosts();
       sinon.assert.calledWith(stub);
       stub.restore();
     });
     it('should return correct posts', () => {
       const expected = sinon.mock();
       const stub = sinon.stub(Post, 'find').returns(expected);
-      const posts = PostService.getAllPosts();
+      const posts = postService.getAllPosts();
       assert.equal(posts, expected);
       stub.restore();
     });
@@ -38,14 +38,14 @@ describe('PostService', () => {
   describe('createNewPost(title, author, body)', () => {
     it('should call function "create" with correct parameters', () => {
       const stub = sinon.stub(Post, 'create');
-      PostService.createNewPost('intro', 'kevin', 'hello');
+      postService.createNewPost('intro', 'kevin', 'hello');
       sinon.assert.calledWith(stub, { title: 'intro', author: 'kevin', body: 'hello' });
       stub.restore();
     });
     it('should return correct promise', () => {
       const expected = sinon.mock();
       const stub = sinon.stub(Post, 'create').returns(expected);
-      const promise = PostService.createNewPost('intro', 'kevin', 'hello');
+      const promise = postService.createNewPost('intro', 'kevin', 'hello');
       assert.equal(promise, expected);
       stub.restore();
     });
