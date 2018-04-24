0b903736423cc7e1a5b53647d7e6778d33708c3d
Kevin Chu <kevinxchu@gmail.com>
2018-04-24 11:00:09 +0900

    return __id field when returning post json object

--- post/service.js
+++ post/service.js
@@ -1,9 +1,9 @@
 const Post = require('../models/Post');
 
 const postService = {
-  getOnePost: postId => Post.findById(postId, { __v: false, _id: false }),
+  getOnePost: postId => Post.findById(postId, { __v: false }),
 
-  getAllPosts: () => Post.find({}, { __v: false, _id: false }),
+  getAllPosts: () => Post.find({}, { __v: false }),
 
   createNewPost: (title, author, body) => Post.create({ title, author, body }),
 };
