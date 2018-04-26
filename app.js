04defee707f03d2d1f7d6f53f57a40b323939ad5
Kevin Chu <kevinxchu@gmail.com>
2018-04-26 10:11:26 +0900

    add noninteractive swagger documentation

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-require('dotenv').config();
 const express = require('express');
 const postController = require('./post/controller');
 const mongoose = require('mongoose');
+require('dotenv').config();
+const swaggerJSDoc = require('swagger-jsdoc');
+const cors = require('cors');
 
 const app = express();
 
@@ -9,10 +11,52 @@ mongoose.Promise = require('bluebird');
 
 mongoose.connect(`mongodb://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@ds257627.mlab.com:57627/first-blog-mern`, { useMongoClient: true });
 
+app.use(cors());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.use('/api', postController);
 
-app.use('', postController);
+const options = {
+  swaggerDefinition: {
+    swagger: '2.0',
+    info: {
+      description: 'Swagger documentation for first-blog-mern',
+      version: '1.0.0', // Version (required)
+      title: 'first-blog-mern-server', // Title (required)
+      termsOfService: 'http://swagger.io/terms/',
+    },
+    host: 'localhost:3000',
+    basePath: '/api',
+    tags: [
+      {
+        name: 'post',
+        description: 'Everything about Posts',
+      },
+    ],
+    schemes: [
+      'http',
+      'https',
+    ],
+    consumes: ['application/json'],
+    produces: ['applicataion/json'],
+  },
+  apis: ['./post/controller.js'], // Path to the API docs
+};
+
+// Initialize swagger-jsdoc => returns validated swagger spec in json format
+const swaggerSpec = swaggerJSDoc(options);
+
+app.get('/api/docs', (req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(swaggerSpec);
+});
 
 app.listen(3000);
 
--- post/controller.js
+++ post/controller.js
@@ -5,6 +5,50 @@ const express = require('express');
 const router = express.Router();
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
+/**
+ *  @swagger
+ *  definitions:
+ *    Post:
+ *      required:
+ *        - "title"
+ *        - "body"
+ *      properties:
+ *        _id:
+ *          type: "string"
+ *          example: "1234567890"
+ *        title:
+ *          type: "string"
+ *          example: "First"
+ *        author:
+ *          type: "string"
+ *          default: "Anonymous"
+ *          example: "Me"
+ *        body:
+ *          type: "string"
+ *          example: "First Post"
+ */
+
+/**
+ *    @swagger
+ *    /posts:
+ *      x-swagger-router-controller: postService
+ *      get:
+ *        tags:
+ *        - "post"
+ *        summary: "Finds all posts"
+ *        operationId: "getAllPosts"
+ *        description: "Returns all posts"
+ *        responses:
+ *          200:
+ *            description: "Success"
+ *            schema:
+ *              type: "array"
+ *              items:
+ *                $ref: "#/definitions/Post"
+ *          503:
+ *            description: "cannot GET /posts"
+ */
+
 router.get(['/', '/posts'], (req, res) => {
   postService.getAllPosts().then((posts) => {
     res.render('posts', { posts });
@@ -12,6 +56,31 @@ router.get(['/', '/posts'], (req, res) => {
     res.status(503).send(err.message);
   });
 });
+
+/**
+ *    @swagger
+ *    /posts/{_id}:
+ *      x-swagger-router-controller: postService
+ *      get:
+ *        tags:
+ *        - "post"
+ *        summary: "Find post by ID"
+ *        operationId: "getOnePost"
+ *        description: "Returns a single post"
+ *        parameters:
+ *        - name: "_id"
+ *          in: "path"
+ *          required: true
+ *          description: "ID of post"
+ *          type: "string"
+ *        responses:
+ *          200:
+ *            description: "successfully found post"
+ *            schema:
+ *              $ref: "#/definitions/Post"
+ *          503:
+ *            description: "cannot GET /posts/post_id"
+ */
 
 router.get('/posts/:post_id', (req, res) => {
   postService.getOnePost(req.params.post_id).then((post) => {
@@ -24,6 +93,32 @@ router.get('/posts/:post_id', (req, res) => {
 router.get('/post_new', (req, res) => {
   res.render('post_new');
 });
+
+/**
+ *    @swagger
+ *    /post_new:
+ *      x-swagger-router-controller: postService
+ *      post:
+ *        tags:
+ *        - "post"
+ *        summary: "Creates a new post"
+ *        operationId: "createNewPost"
+ *        description: "Returns a new post"
+ *        parameters:
+ *        - name: "body"
+ *          in: "body"
+ *          required: true
+ *          schema:
+ *            $ref: "#/definitions/Post"
+ *        responses:
+ *          200:
+ *            description: "successfully created new post"
+ *            schema:
+ *              $ref: "#/definitions/Post"
+ *          503:
+ *            description: "cannot POST /posts"
+ *
+ */
 
 router.post('/post_new', urlencodedParser, (req, res) => {
   postService.createNewPost(req.body.title, req.body.author, req.body.body).then(() => {
