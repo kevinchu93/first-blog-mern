ab8358baab55e78e9f554089a30498492cb23ce9
Kevin Chu <kevinxchu@gmail.com>
2018-05-23 13:11:57 +0900

    add env variables, add default port

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const app = express();
 
 mongoose.Promise = require('bluebird');
 
-mongoose.connect(`${process.env.DATABASE}://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.HOST}`, { useMongoClient: true });
+mongoose.connect(`mongodb://${process.env.DATABASE_USER}:${process.env.DATABASE_PASS}@${process.env.DATABASE_HOST}:${process.env.DATABASE_PORT}/${process.env.DATABASE}`, { useMongoClient: true });
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.use('', postController);
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
